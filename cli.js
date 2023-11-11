c4a89f20a950472461666856598e9a5190e34e4d
Shah Kashani <skashani@gmail.com>
2023-11-11 17:48:30 +0100

    They are back

--- cli.js
+++ cli.js
@@ -8,24 +8,19 @@ const {
   CONSUMER_KEY,
   CONSUMER_SECRET,
   BLOG_NAME,
-  BANNED_WORDS,
-  CORMORANTS_MODEL_NAME,
-  CORMORANTS_CORPUS,
 } = process.env;
 
 const mysteries = new Cormorants({
-  accessTokenKey: ACCESS_TOKEN_KEY,
-  accessTokenSecret: ACCESS_TOKEN_SECRET,
-  consumerKey: CONSUMER_KEY,
-  consumerSecret: CONSUMER_SECRET,
-  blogName: BLOG_NAME,
-  modelName: CORMORANTS_MODEL_NAME,
-  corpus: CORMORANTS_CORPUS,
-  isVerbose: true,
-  bannedWords: (BANNED_WORDS || '').split(','),
+  tumblrConfig: {
+    accessTokenKey: ACCESS_TOKEN_KEY,
+    accessTokenSecret: ACCESS_TOKEN_SECRET,
+    consumerKey: CONSUMER_KEY,
+    consumerSecret: CONSUMER_SECRET,
+    blogName: BLOG_NAME,
+  },
 });
 
 (async function () {
-  console.log(await mysteries.speak());
+  console.log(await mysteries.getNextAsk());
   process.exit(0);
 })();
--- index.js
+++ index.js
@@ -1,52 +1,40 @@
-const { map, sample, upperFirst, compact, countBy } = require('lodash');
 const tumblr = require('tumblr.js');
-const { exec } = require('shelljs');
+
+const TYPE_ASK = 'ask';
+const TYPE_IMAGE = 'image';
+const TYPE_TEXT = 'text';
 
 class Cormorants {
   constructor({
-    consumerKey,
-    consumerSecret,
-    accessTokenKey,
-    accessTokenSecret,
-    blogName,
-    corpus,
-    modelName = 'deepset/bert-base-cased-squad2',
-    maxCorpusLength = 10000,
-    minWords = null,
-    isVerbose = false,
-    isIncludeMedia = false,
-    filterText = null,
-    setQuestion = null,
-    setAnswer = null,
-    moderation = null,
+    tumblrConfig: {
+      consumerKey,
+      consumerSecret,
+      accessTokenKey,
+      accessTokenSecret,
+      blogName,
+    },
   }) {
     this.client = tumblr.createClient({
       consumer_key: consumerKey,
       consumer_secret: consumerSecret,
       token: accessTokenKey,
       token_secret: accessTokenSecret,
-      returnPromises: true,
     });
-    this.modelName = modelName;
-    this.corpus = corpus;
-    this.maxCorpusLength = maxCorpusLength;
     this.blogName = blogName;
-    this.isVerbose = isVerbose;
-    this.isIncludeMedia = isIncludeMedia;
-    this.filterText = filterText;
-    this.setQuestion = setQuestion;
-    this.setAnswer = setAnswer;
-    this.minWords = minWords;
-    this.moderation = moderation;
   }
 
-  async filter(arr, callback) {
-    const fail = Symbol();
-    return (
-      await Promise.all(
-        arr.map(async (item) => ((await callback(item)) ? item : fail))
-      )
-    ).filter((i) => i !== fail);
+  getRest(post) {
+    const { layout, content } = post;
+    const ask = layout.find(({ type }) => type === TYPE_ASK);
+    if (!ask) {
+      return null;
+    }
+    return content.filter((_c, i) => ask.blocks.indexOf(i) === -1);
+  }
+
+  filter(post) {
+    const rest = this.getRest(post);
+    return !rest || !rest.find(({ type }) => type === TYPE_IMAGE);
   }
 
   async posts() {
@@ -54,153 +42,38 @@ class Cormorants {
     let isTraversing = true;
     let beforeId = undefined;
     while (isTraversing) {
-      if (this.isVerbose) {
-        console.log(
-          `🦅  Getting posts${beforeId ? ` (before ID ${beforeId})` : '...'}`
-        );
-      }
-      const { posts, _links } = await this.client.blogSubmissions(
-        this.blogName,
-        {
-          npf: true,
-          limit: 50,
-          before_id: beforeId,
-        }
-      );
-      results.push.apply(results, posts);
+      const { posts, _links } = await this.client.blogDrafts(this.blogName, {
+        npf: true,
+        limit: 50,
+        before_id: beforeId,
+      });
+      const asks = posts.filter((p) => this.filter(p));
+      results.push.apply(results, asks);
       if (posts.length === 0 || !_links || !_links.next) {
         isTraversing = false;
       } else {
         beforeId = _links.next.query_params.before_id;
       }
     }
-    return this.filter(results, (t) => this.filterFn(t));
+    return results;
   }
 
-  async filterFn(post) {
-    const text = this.question(post);
-    if (!this.isIncludeMedia) {
-      if (post.content.some(({ type }) => type !== 'text')) {
-        if (this.isVerbose) {
-          console.log(`🦅 Skipping [media]: ${text}`);
-        }
-        return false;
-      }
-    }
-    if (this.filterText) {
-      if (text.toLowerCase().indexOf(this.filterText.toLowerCase()) === -1) {
-        if (this.isVerbose) {
-          console.log(`🦅 Skipping ["${this.filterText}"]: ${text}`);
-        }
-        return false;
-      }
-    }
-    if (this.moderation) {
-      if (!(await this.moderation.validate(text))) {
-        if (this.isVerbose) {
-          console.log(`🦅 Skipping [profanity]: ${text}`);
-        }
-        return false;
-      }
-    }
-    return true;
+  getCaptions(post) {
+    return this.getRest(post)
+      .filter(({ type }) => type === TYPE_TEXT)
+      .map(({ text }) => text);
   }
 
-  images(post) {
-    const images = post.content.filter(({ type }) => type === 'image');
-    return images.map((image) => {
-      const { type, url, width, height } = image.media[0];
-      return { type, url, width, height };
-    });
-  }
-
-  question(post) {
-    const line = compact(map(post.content, 'text'))
-      .join(' ')
-      .replace(/\s{2,}/g, ' ');
-    const clean = line.trim().replace(/[.,?:]$/, '');
-    return `${clean}?`;
-  }
-
-  async answer(question) {
-    const cleanQ = question.replace(/(["])/g, '\\$1');
-    // Truly batshit crazy, but Tensorflow versions step all over each other so we need to sandbox it.
-    const cmd = `node "${__dirname}/ask.js" "${this.modelName}" "${cleanQ}" "${this.corpus}" ${this.maxCorpusLength}`;
-    const result = exec(cmd, { silent: false });
-    if (result.code !== 0) {
-      throw new Error(`Shell command error: ${result.stderr.trim()}\n> ${cmd}`);
-    }
-    const stdout = result.stdout.trim();
-    const matches = stdout.match(/<answer>([\s\S]*)<\/answer>/);
-    const text = matches ? matches[1] : '';
-    if (
-      (this.moderation && !(await this.moderation.validate(text))) ||
-      (this.minWords && text.split(/\s/).length < this.minWords)
-    ) {
-      return await this.answer(question);
-    }
-    return this.toSentence(text);
-  }
-
-  count(str, ch) {
-    return countBy(str)[ch] || 0;
-  }
-
-  toClean(text) {
-    const bookends = [
-      ['“', '”'],
-      ['(', ')'],
-      ['[', ']'],
-    ];
-    const doubles = ['"'];
-    let sanitized = text.trim();
-    bookends.forEach(([open, close]) => {
-      if (sanitized.indexOf(open) === -1 || sanitized.indexOf(close) === -1) {
-        sanitized = sanitized.replace(open, '').replace(close, '');
-      }
-    });
-    doubles.forEach((double) => {
-      if (this.count(sanitized, double) === 1) {
-        sanitized = sanitized.replace(double, '');
-      }
-    });
-    return sanitized;
-  }
-
-  toSentence(rawString) {
-    if (!rawString) {
-      return '';
-    }
-    const string = this.toClean(rawString);
-    const punc = new RegExp(/[.?!]$/).test(string) ? '' : '.';
-    const clean = string.replace(/[\n\r]/g, ' ').replace(/\s{2,}/g, ' ');
-    return `${upperFirst(clean)}${punc}`;
-  }
-
-  getBaseParams(apiPath) {
-    return {
-      ...this.client.requestOptions,
-      url: this.client.baseUrl + apiPath,
-      oauth: this.client.credentials,
-    };
-  }
-
-  async speak() {
+  async getNextAsk() {
     const asks = await this.posts();
     if (asks.length === 0) {
       return null;
     }
-    const ask = sample(asks);
-    console.log(`🦅 Ask: ${this.question(ask)}`);
-    console.log(JSON.stringify(ask, null, 2));
-    const question = this.setQuestion || this.question(ask);
-    const answer = this.setAnswer || (await this.answer(question));
-    const images = this.images(ask);
+    const [ask] = asks;
+    const captions = this.getCaptions(ask);
     return {
-      images,
       ask,
-      question,
-      answer,
+      captions,
     };
   }
 }
